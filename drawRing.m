bb484137f599b592609fac758a6e78786a3577a9
Will Foran <willforan@gmail.com>
2014-05-27 14:09:54 -0400

    init

---
+++ drawRing.m
@@ -0,0 +1,120 @@
+function StimulusOnsetTime = drawRing(w,numStim, varargin)
+   % globals used by all functions
+   global colors degsize;
+   % colors =  rgb for black, purple, green, light blue, pink, red, yellow, white
+ 
+    
+   % small grid should be 5 x 5 degsize and centered
+   % so upper left is the center minus half the total pixels of the grid
+   upperLeft= (paren(Screen('Rect',w),[3,4]) - 5*degsize)/2;
+   
+   
+   
+   % cricles are 65% of a degree
+   crclSize= degsize*.65 ;
+   
+   % top left corner of each circle
+   positions=  [ [1 3 0 4 1 3 ]'.* degsize + upperLeft(1)  ... grid spaces from left
+                 [0 0 2 2 4 4 ]'.* degsize + upperLeft(2)  ... grid spaces from top
+                 ];
+             
+
+    
+    % we use the following positions to draw a rectangle that hides
+    % part of the circle and indicates which way a saccade should go
+    %  ordered: left right up down
+    movement = { ...
+        ... LEFT
+        [  [0          crclSize/6], ...
+           [crclSize/2 5*crclSize/6] ] ...
+         ... RIGHT
+         [ [crclSize-crclSize/2 crclSize/6  ], ...
+           [crclSize            5*crclSize/6] ] ...
+         ... UP
+         [  [crclSize/6   0              ],  ...
+            [5*crclSize/6 crclSize/2    ] ]...
+         ... DOWN
+         [  [crclSize/6   crclSize-crclSize/2 ], ...
+            [5*crclSize/6 crclSize            ] ]...
+    };
+       
+  % which direction to move in
+  Directions = randi(4,1,numStim);
+
+  % maybe we want to assing 
+  % a specific (1) position with a (2) color and a (3) direction
+  %  like:
+  %   drawRing(w,100,6, 'Position', 2, 'Color', 3, 'Direction', 1)
+  %% Get options
+  labeledPosIdx = find(cellfun(@(x) ischar(x)&&strcmpi(x,'Position'), varargin ))+1;
+  labeledColIdx = find(cellfun(@(x) ischar(x)&&strcmpi(x,'Color'), varargin ))+1;
+  labeledDirIdx = find(cellfun(@(x) ischar(x)&&strcmpi(x,'Direction'), varargin ))+1;
+  whenIdx       = find(cellfun(@(x) ischar(x)&&strcmpi(x,'when'), varargin ))+1;
+
+  % what's left to be random
+  if ~isempty(labeledPosIdx)
+     randPos = setdiff( 1:numStim , labeledPosIdx ); % positions that will be randomly set
+  else
+      randPos = 1:numStim;
+  end
+  
+    
+  % if we have colors specified, set them and suffle the rest
+  if ~isempty(labeledColIdx)
+      % shuffle the list of unused colors, take only the number we need
+      ColorIdxs(randPos) = paren( Shuffle( setdiff( 1:length(colors), varargin{labeledColIdx}) ), 1:length(randPos) );
+      ColorIdxs(varargin{labeledPosIdx}) = varargin{labeledColIdx};
+  % otherwise just shuffle the colors
+  else
+      ColorIdxs = Shuffle(1:length(colors)  );
+  end
+  
+  % if we have directions specified, set them
+  if ~isempty(labeledDirIdx)
+        Directions(varargin{labeledPosIdx}) = varargin{labeledDirIdx};
+  end
+  
+  
+
+  %% draw 'em 
+   for n = 1:numStim
+         % draw ovel
+         Screen('FrameOval', w , colors(ColorIdxs(n),:),   ... color
+              [ positions(n,:), positions(n,:)+crclSize ], ... position 
+              degsize.*.05                                 ... pen width
+             );
+         
+
+ 
+         % if this is the probe, we want to indicate direction
+         if( ~isempty( find(cellfun(@(x) ischar(x)&&strcmpi(x,'PROBE'), varargin )) ))
+            % what direction are we going to show
+            movmat = movement{Directions(n)};
+            % cut out piece on left or right
+            Screen('FillRect', w ,[1 1 1]*255/2,  ... color--same as background
+                 [ positions(n,:) + movmat(1:2)   ... top left
+                   positions(n,:) + movmat(3:4)], ... bottom right 
+                 degsize.*.05                     ... pen width
+               );
+         end
+   end
+   
+   
+   if ~isempty(whenIdx)
+      when=varargin{whenIdx};
+   else
+      when=now();
+   end
+   
+   [VBLTimestamp StimulusOnsetTime  ] = Screen('Flip',w,when);
+   
+   KbWait;
+end
+
+function r=paren(x, varargin)
+  r=x(varargin{:});
+end
+
+function r=curly(x, varargin)
+ r=x{varargin{:}};
+end
---
+++ popout.m
@@ -0,0 +1,99 @@
+%% draw a popout screen
+%    given a screen and a degree size
+% 
+% display is 7° x 7° grid with
+%   up to 6 stimuli (0.65° annuli -- circle or notched circle)
+%   on a centered 5x5 grid
+%   
+%   each colored distincly
+%     black, purple, green, light blue, pink, red, yellow, or white
+% 
+% 1. fix (.5 sec)
+% 2. cue (.5 secs)
+% 3. attention (.5secs)
+% 4. probe (.5secs) + wait for response (<= 1.5s)
+
+
+function popout(varargin)
+    global colors degsize;
+   % black, purple, green, light blue, pink, red, yellow, white
+   colors = [ 0   0   0;
+              255 0   255;
+              0   255 0;
+              173 216 255; 
+              255 173 173;
+              255 255 0;
+              255 192 203;
+              255 255 255];
+          
+          
+    % each degree is 100 pixels   
+    degsize=100;
+    
+    try
+
+      w = setupScreen();
+      
+      colorIDX = 1;
+      %% 0. fixation
+      %timing.fixation.onset = fixation(w,now()+.5);
+      
+      %% 1. draw cue
+      %timing.cue.onset = cue(w,colorIDX);
+      
+      %% 2. draw attention
+      timing.attention.onset = drawRing(w,6, 'Position',2,'Color',colorIDX);
+      
+      
+      %% 3. draw probe
+      timing.probe.onset = drawRing(w,6,'PROBE', 'Position', 3, 'Color', colorIDX, 'Direction', 1);
+      %attention(w,100,6, 'Position', [1,2], 'Color',[1,2],'Direction',[1,1], 'when', now())
+      
+      %% 4. response
+      %  waiting for input, blank screen in 500ms
+
+   
+    catch
+       % panic? close all
+       psychrethrow(psychlasterror);
+       closedown();
+    end
+   
+    closedown();
+end
+
+%% setup screen
+function w=setupScreen()
+         % 50% grey
+         backgroundColor=[1 1 1].*256.*.5;
+         % Find out how many screens and use smallset screen number.
+         screenResolution = [800 600];
+         screennum=max(Screen('Screens'));
+         %wSize=Screen('Resolution', screennum);
+         %screenResolution = [wSize.width wSize.height];
+         
+         
+         % Removes the blue screen flash and minimize extraneous warnings.
+         % http://psychtoolbox.org/FaqWarningPrefs
+         Screen('Preference', 'Verbosity', 2); % remove cli startup message 
+         Screen('Preference', 'VisualDebugLevel', 3); % remove  visual logo
+         %Screen('Preference', 'SuppressAllWarnings', 1);
+
+         % Open a new window.
+         w = Screen('OpenWindow', screennum,backgroundColor, [0 0 screenResolution] );
+         % [ w, windowRect ] = Screen('OpenWindow', max(Screen('Screens')),[ 204 204 204], [] );
+
+         %permit transparency
+         Screen('BlendFunction', w, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+         
+         % set font
+         Screen('TextFont', w, 'Arial');
+         Screen('TextSize', w, 22);
+end
+
+function closedown()
+     ShowCursor;
+     %ListenChar(0);
+     Screen('CloseAll');
+     Priority(0); % set priority to normal
+end
