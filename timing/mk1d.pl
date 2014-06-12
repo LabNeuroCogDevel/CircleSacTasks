022da6cd2dc14a75a6ba2748472143d921b98cbc
Will Foran <willforan@gmail.com>
2014-06-12 07:24:52 -0400

    handle catch trials, calculate occur and trial seq time

--- timing/mk1d.pl
+++ timing/mk1d.pl
@@ -2,6 +2,7 @@
 use strict; use warnings;
 use Data::Dumper;
 use 5.14.0;
+use List::Util qw(sum reduce);
 
 # example input "snd[.5]; mem {1L=1/2 [.5], 4L=1/2 [.5] }; CATCH=1/6; dly[1];"
 
@@ -14,32 +15,30 @@ while(<>) {
  # read in event by event
  for (@F) {
    # skip if it doesnt look like a name and time
-   next if ! /\w+.*\[/; 
+   next if ! /(\w+.*\[)|(CATCH)/; 
 
    my $name;
    my @cond=();
    my @timing=();
 
-   print "# $_\n";
 
    # DEAL WITH CATCH TRIALS
    if(/ CATCH \s*=\s* ( [\d\.\/]+ )/x){
      $nCatch++;
-     my $occur=$1;
+     my $occur=sprintf('%0.4f',$1);
      $name="CATCH".$nCatch;
-     say "$name";
 
      @cond=( [ $name, $occur ], ["NO".$name, 1-$occur] );
      @timing=(0, 0);
 
-     $events{$name} = [ map { { event=>$name, name=>$cond[$_]->[0], occurRatio=>$cond[$_]->[0], duration=>$timing[$_] } } (0..$#cond)  ];
+     $events{$name} = [ map { { event=>$name, name=>$cond[$_]->[0], occurRatio=>$cond[$_]->[1], duration=>$timing[$_] } } (0..$#cond)  ];
+     push @seq, $name;
      next;
    }
 
    # get name, should be first bit of word characters
    /^\s*(?<name>\w+)/;
    $name=$+{name};
-   print "using $name\n";
    say STDERR "no name for event!" and exit unless $name;
    say STDERR "name $name already used!" and exit if exists($events{$name});
    push @seq, $name;
@@ -67,7 +66,10 @@ while(<>) {
    # build events hash
    # eventName =>  array of hashes with prefix, occurRatio, and duration
    if( ( $#cond<0 && $#timing==0) || ($#cond == $#timing) ) {
-      $events{$name} = [ map { { event=>$name, name=>$cond[$_]->[0], occurRatio=>$cond[$_]->[0], duration=>$timing[$_] } } (0..$#cond)  ];
+      $events{$name} = [ map { { event=>$name, name=>$cond[$_]->[0], 
+                                 occurRatio=>sprintf("%.04f",$cond[$_]->[1]), 
+                                 duration=>sprintf("%.02f",$timing[$_])      } 
+                              } (0..$#cond)  ];
    } else {
      say STDERR "$name: have " , $#timing+1 , " times -- should be 1 or match # manipulations\n";
    }
@@ -80,9 +82,9 @@ while(<>) {
 # seq like: 
 #  (snd mem dly)
 # events like:
-#  ( snd => [ {event=>snd,name=>snd,    occurRatio=>1  , duration=>.5 ],
-#    mem => [ {event=>mem,name=>mem.1L, occurRation=>.5, duration=>.5},
-#             {event=>mem,name=>mem.4L, occurRation=>.5, duration=>.5}
+#  ( snd => [ {event=>snd,name=>snd,    occurRatio=>1 , duration=>.5 ],
+#    mem => [ {event=>mem,name=>mem.1L, occurRatio=>.5, duration=>.5},
+#             {event=>mem,name=>mem.4L, occurRatio=>.5, duration=>.5}
 #           ]
 #  )          
 
@@ -108,9 +110,7 @@ for my $event (@seq) {
         my @newseq = @{$prevseq[$i]};
         
         # don't extend trials that end it catch
-        next if $#newseq>0 && $newseq[$#newseq]->{name} =~ /^CATCH\d+$/; 
-
-        push @newseq, $condition;
+        push @newseq, $condition unless $#newseq>0 && $newseq[$#newseq]->{name} =~ /^CATCH\d+$/; 
         push @allseq, [@newseq ];
       }
    }
@@ -118,5 +118,7 @@ for my $event (@seq) {
 }
 
 for my $trialseq (@allseq) {
- say join("\t",map {$_->{name} . $_->{duration}} @$trialseq);
+ my $occur = reduce {$a*$b} 1, map {$_->{occurRatio} } @$trialseq;
+ my $time  = reduce {$a+$b} 0, map {$_->{duration}   } @$trialseq;
+ say "$occur @ $time s :", join("\t",map {"$_->{name} $_->{duration}s"} @$trialseq);
 }
