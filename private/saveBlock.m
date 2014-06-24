1688385e13bc3ab9056e906fa6a8e5899455434c
Will Foran <willforan@gmail.com>
2014-06-24 10:20:37 -0400

    track save block

---
+++ private/saveBlock.m
@@ -0,0 +1,9 @@
+function saveBlock(subject, thisBlk,startofblock,endofblock)
+     saveblockfname = [ subject.file '_blk' num2str(thisBlk) '_'  subject.runtime((end-4):end) ];
+     blockevents.events     = subject.events( startofblock:endofblock );
+     blockevents.trial      = subject.trial( startofblock:endofblock );
+     blockevents.waitbefore =  subject.waitbefore( startofblock:endofblock );
+     blockevents.starttime  = subject.starttime(thisBlk);
+     blockevents.endttime   = subject.endtime(thisBlk);
+     save(saveblockfname,'-struct', 'blockevents');
+end
