bcf01557d0741baa9df0a73221a547fc00ff7f70
Will Foran <willforan@gmail.com>
2014-10-21 10:21:40 -0400

    add timing for noCatch and 500mem

--- timing/getBestWorkingMemory.bash
+++ timing/getBestWorkingMemory.bash
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-for d in workingMemory_vardly; do # workingMemory workingMemory_vardly; do
+for d in workingMemory_vardly workingMemory_noCatch workingMemory_500mem; do # workingMemory workingMemory_vardly; do
    echo $d
    cd $d/
    count=1
+
+   [ -d best ] && rm -r  best
+   mkdir best
+
    for i in $(sort -k3,3n stimSums.txt | cut -f 1|sed '1d;7q'); do
       echo "	cp stims/$i/alltiming.txt  best/$count.txt"
       cp stims/$i/alltiming.txt best/$count.txt
