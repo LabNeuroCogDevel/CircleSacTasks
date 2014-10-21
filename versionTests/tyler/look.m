db773180cff1038f24765972a4992a95ae55d3a5
Will Foran <willforan@gmail.com>
2014-10-21 14:26:26 -0400

    update look script

--- versionTests/tyler/look.m
+++ versionTests/tyler/look.m
@@ -1,15 +1,16 @@
-files=dir('versionTests/tyler/');
-for f=3:length(files)
-   r(f-2)=WMBehav(['versionTests/tyler/' files(f).name],1:48)
+files=dir('versionTests/tyler/*mat');
+for f=1:length(files)
+   r(f)=WMBehav(['versionTests/tyler/' files(f).name],1:48)
 end
 
 for i=1:length(r)
-  r(i).subj,
+  disp(r(i).subj)
   isLowLoad=r(i).matrix(:,3)==1;
-  fprintf('all\n');
-  disp([ histc( r(i).matrix(:,1) , -1:1 ), [-1:1]'])
-  fprintf('low\n');
-  disp([ histc( r(i).matrix(isLowLoad,1) , -1:1 ), [-1:1]'])
-  fprintf('high\n');
-  disp( [ histc( r(i).matrix(~isLowLoad,1) , -1:1 ), [-1:1]']);
+
+  rng=[-1:1]';
+  allsc=histc( r(i).matrix(  :       ,1) , rng );
+  low  =histc( r(i).matrix(isLowLoad ,1) , rng );
+  high =histc( r(i).matrix(~isLowLoad,1) , rng );
+  fprintf('\tall\tlow\thigh\n')
+  disp([ rng, allsc,low,high]),
 end
