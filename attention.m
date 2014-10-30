698710e070cd3a40991b7921b0458504ba5b120d
Will Foran <willforan@gmail.com>
2014-10-30 17:10:14 -0400

    ATT: back to .5 and 2 for attend and rsp, add att settings file

--- attention.m
+++ attention.m
@@ -109,11 +109,10 @@
 function subject = attention(varargin)
    %% globals
    % colors, paren, and degsize defined in setupscreen
-   global TIMES listenKeys CUMULATIVE CLEARTIME modality filelist;
-   %       fix->cue->attend->probe->clear  
-   TIMES = [  .5   .5      .5     .5 ]; % time between each event in seconds
-   CLEARTIME = 1.5; % additional time to response after clearing the screen
-   shrinkVal=1/2; % how big is the hole in the circles on probe
+   global TIMES listenKeys CUMULATIVE modality filelist shrinkVal;
+   % TIMES, CLEARTIME, and shrinkVal set in 
+   ATTsettings();
+   
    % shrinkVal calc like (in drawRing.m)
    %annulisSize = crclSize/2 + [-1;1].*crclSize/2 *shrinkProbe;
    %movement = { ...
